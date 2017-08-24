6aeca4207689e662eafdc6d32e825a99e1c61aa8
Priya Balakrishnan <andu_pri@yahoo.com>
2017-08-24 17:56:46 -0400

    feat: Display total wins and losses in game

--- assets/javascript/app-trial.js
+++ assets/javascript/app-trial.js
@@ -21,18 +21,57 @@ var model = {
 				{	question: "What state contains the most national parks?", 
 	 			  	choices: ["assets/images/Colorado.jpg", "assets/images/Utah.jpg", "assets/images/Alaska.jpg", "assets/images/California.jpg"], 
 					altText: ["Colorado", "Utah", "Alaska", "California"], 
-					answer: "Alaska", 
+					answer: "California", 
+					position: 3,
+				},
+				{	question: "Through how many states does the Appalachian National Scenic Trail pass?", 
+	 			  	choices: ["assets/images/number-10.jpg", "assets/images/number-14.jpg", "assets/images/number-9.jpg", "assets/images/number-twelve.jpg"], 
+					altText: ["10", "14", "9", "12"], 
+					answer: "14", 
+					position: 1,
+				},
+				{
+					question: "What is the most visited national park?",
+					choices: ["assets/images/Yosemite-1.jpg", "assets/images/grand-canyon.jpg", "assets/images/Great-Smoky-Mountains.jpg", "assets/images/Rocky-Mountain.jpg"], 
+					altText: ["Yosemite National Park", "Grand Canyon National Park", "Smoky Mountains National Park", "Rocky Mountain National Park"], 
+					answer: "Smoky Mountains National Park", 
 					position: 2,
 				},
+				{
+					question: "Which of the following is not one of Utah's 'Mighty 5' National Parks?",
+					choices: ["assets/images/arches.jpg", "assets/images/capitol-reef.jpg", "assets/images/bryce-canyon.jpg", "assets/images/badlands.jpg"], 
+					altText: ["Arches National Park", "Capitol Reef National Park", "Bryce Canyon National Park", "Badlands National Park"], 
+					answer: "Badlands National Park", 
+					position: 3,
+				},
+				// {	question: "This man is considered 'The Father of the National Parks.'",
+				// 	choices: ["assets/images/John-Muir.jpg", "assets/images/Teddy-Roosevelt.jpg", "assets/images/Joseph-LeConte.jpg", "assets/images/Ralph-Waldo-Emerson.jpg"], 
+				// 	altText: ["John Muir", "Teddy Roosevelt", "Joseph LeConte", "Ralph Waldo Emerson"], 
+				// 	answer: "John Muir", 
+				// 	position: 0,
+				// },
+				// {	question: "Who was the only U.S. President to work as a park ranger?",
+				// 	choices: ["assets/images/Jimmy-Carter.jpg", "assets/images/Teddy-Roosevelt-1.jpg", "assets/images/Gerald-Ford.jpg", "assets/images/harry-truman.jpg"], 
+				// 	altText: ["Gerald Ford", "Teddy Roosevelt", "Jimmy Carter", "Harry S. Truman"], 
+				// 	answer: "Gerald Ford", 
+				// 	position: 2,
+				// },
+				// {	question: "What National Park contains the highest peak in North America?",
+				// 	choices: ["assets/images/mount-rainier.jpg", "assets/images/Grand-Teton.jpg", "assets/images/Denali.jpg", "assets/images/hawaii-volcanoes.jpg"], 
+				// 	altText: ["Mount Rainier National Park", "Grand Teton National Park", "Denali National Park", "Hawaii Volcanoes National Park"], 
+				// 	answer: "Denali National Park", 
+				// 	position: 2,
+				// }
 				],
 				time: 30,
 				intervalId: 0,
 				objIndex: 0,
+				wins: 0,
+				losses: 0,
 				
 				reset: function() {
-					console.log("inside reset");
-	    			model.time = 30;
-	    			intervalId = 0;
+					model.time = 30;
+	    			model.intervalId = 0;
 	    			// Change the "display" div to "00 seconds.
 	    			$("#display").html("30 Seconds");
   				},
@@ -66,22 +105,20 @@ var model = {
  				},
 
  				next: function() {
- 					console.log("inside next");
  					if(this.objIndex < model.questions.length -1){
- 						console.log(this.objIndex);
-	 					this.objIndex++;
+ 						this.objIndex++;
 	 					//return this.questions[this.objIndex];
 	 					setTimeout(gameView, 5000, this.questions[this.objIndex]);
+ 				 	}else {
+ 				 		setTimeout(showResult, 5000);
  				 	}	
  				},
 	}
 
-
-//prevents the clock from being sped up unnecessarily
-var clockRunning = false;
-
-// Variable for indicating game success
-var success = false;
+function showResult() {
+	$("#question").html("<h3>You have " + model.wins + " correct answers and "+ model.losses+" wrong answers.</h3>"); 
+ 	$("#question").append("<button id='restart' class='btn btn-success'>Restart Game</button>");
+}
 
 function gameView(obj) {
 	model.start();
@@ -90,6 +127,7 @@ function gameView(obj) {
 	newDiv.addClass("row trivia");
 	newDiv.data("answer", obj.answer);
 	newDiv.data("image", obj.choices[obj.position]);
+	
 	for(var i=0; i<obj.choices.length;i++) {
 		var newCol = $("<div>");
 		newCol.addClass("col col-md-2 text-center");
@@ -118,16 +156,16 @@ $(document).ready(function() {
 	 	model.stop();
 	 	model.reset();
 	  	if($(".trivia").data("answer") === $(this).text()){
-	 		$("#question").html('<h3>You got it!</h3></br><img src="'+$(".trivia").data("image")+'">');
+	 		$("#question").html('<h3>You got it! '+$(".trivia").data("answer")+' is correct.</h3></br><img src="'+$(".trivia").data("image")+'">');
 	 		//sucess = true;
+	 		model.wins++;
 	 		model.next();
 	 	}else {
 	 		$("#question").html('<h3>Sorry! The correct answer is ' + $(".trivia").data("answer")+'</h3>'+'</br><img src="'+$(".trivia").data("image")+'">');
 	 		//sucess = true;
+	 		model.losses++;
 	 		model.next();
 	 	}
 	});
 });	
 
-// Options
-//Refractor the object looping in a function and call it for on load and after sucess or failure of game. 
